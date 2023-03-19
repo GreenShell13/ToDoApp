83a131456824b40ab7889dfb5351808f524efe65
GreenShell13 <vlad_samokatov@mail.ru>
2023-03-19 13:25:24 +0300

    Topics on board

--- front/src/pages/materials/Materials.jsx
+++ front/src/pages/materials/Materials.jsx
@@ -1,7 +1,19 @@
 import React, { memo } from 'react';
+import './topics-style.css';
+
+import topics from './topics';
 
 const Materials = () => {
-    return <h1>Materials</h1>
+    return <>
+        <h1>Materials</h1>
+        <div className="topics">
+            {topics.map((topic, key) => 
+                <div className="topic-item" key={key}>
+                    {topic.label}
+                </div>
+            )}
+        </div>
+    </>
 };
 
 export default memo(Materials);
---
+++ front/src/pages/materials/topics.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 0,
+        label: 'Front-end'
+    },
+    {
+        id: 1,
+        label: 'Back-end'
+    },
+    {
+        id: 2,
+        label: 'DevOps'
+    },
+    {
+        id: 3,
+        label: 'AI'
+    },
+    {
+        id: 4,
+        label: 'Computer Science'
+    }
+];
