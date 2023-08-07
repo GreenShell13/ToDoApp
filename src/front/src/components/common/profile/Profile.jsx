d727aeabf669e6448719d30d00feec3d33f4701a
GreenShell13 <vlad_samokatov@mail.ru>
2023-08-08 00:42:11 +0300

    Dockerization

---
+++ src/front/src/components/common/profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react';
+
+const Profile = ({ src = '', alt= 'Here should be a picture' }) => {
+    return (
+        <img />
+    );
+};
+
+export default memo(Profile);
