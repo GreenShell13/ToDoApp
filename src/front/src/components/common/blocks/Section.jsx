eff065844e5dd5e2ea1ee424a1f19bccc1ec9bbc
GreenShell13 <vlad_samokatov@mail.ru>
2023-08-10 15:53:47 +0300

    Home title styled

--- src/front/src/components/common/blocks/Section.jsx
+++ src/front/src/components/common/blocks/Section.jsx
@@ -5,16 +5,39 @@ import MountainLake from '../../../assets/images/mountain-lake.avif'
 
 const Wrap = styled.div`
     flex-grow: 1;
+    width: 100vw;
+    height: 100vh;
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
     background-image: url(${MountainLake});
 `
 
+const ItemText = styled.div`
+    margin-top: 5vh;
+    margin-left: 32vw;
+    width: 36vw;
+    height: 15vh;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    background-color: #E0E0E0;
+    align-items: center;
+    justify-content: center;
+    border-radius: 100px;
+    font-size: 1rem;
+    opacity: 0.8;
+`
+
 const Section = () => {
-    return <Wrap>
-        <div>Section</div>
-    </Wrap>
+    return (
+        <Wrap>
+            <ItemText>
+                <h1 style={{ margin: 0, fontSize: '3rem' }}>Электронный Ежедневник</h1>
+                <p>Планируй свой день и развивайся в своё удовольствие!</p>
+            </ItemText>   
+        </Wrap>
+    )
 }
 
 export default memo(Section)
--- src/front/src/pages/home/Home.jsx
+++ src/front/src/pages/home/Home.jsx
@@ -4,8 +4,11 @@ import Section from '../../components/common/blocks/Section'
 
 const Container = styled.div`
     height: 100vh;
+    width: 100vvw;
     display: flex;
     flex-direction: column;
+    text-align: center;
+    align-items: center;
 `
 
 const Home = () => {
