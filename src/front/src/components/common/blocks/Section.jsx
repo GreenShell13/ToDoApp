85541491941386329b6f7b1fd62ab66deefb8cf9
GreenShell13 <vlad_samokatov@mail.ru>
2023-08-13 21:22:17 +0300

    Auth divides from Section

--- src/front/src/components/common/blocks/Section.jsx
+++ src/front/src/components/common/blocks/Section.jsx
@@ -1,24 +1,15 @@
-import React, { memo, useCallback } from 'react'
+import React, { memo } from 'react'
 import Fade from 'react-reveal/Fade'
-import { useDispatch, useSelector } from 'react-redux'
-import axios from 'axios'
-
-import { TextField }from '@mui/material'
+import { useSelector } from 'react-redux'
 
 import { 
     Wrap, 
     ItemText, 
-    AuthButton,
     TopBar,
-    ButtonGroup,
-    RegButton,
-    AuthForm
 } from './styledComponents'
-import { 
-    authFormSelector, 
-    updateAuthParam 
-} from '../../../pages/home/reducer/slice'
-import { authUrl } from '../../../pages/home/reducer/constants'
+import AuthForm from '../../auth/AuthForm'
+import { authFormSelector } from '../../../pages/home/reducer/slice'
+
 
 const Section = ({
     title = '',
@@ -27,19 +18,7 @@ const Section = ({
     bgimg = '',
     styles = { color: '#fff' }
 }) => {
-    const dispatch = useDispatch()
-    const {login, password, isAuth } = useSelector(authFormSelector)
-
-    const handleChange = useCallback(e => 
-        dispatch(updateAuthParam({ [e.target.name]: e.target.value }))
-    , [dispatch])
-
-    const handleClick = useCallback(async () => {
-        await 
-            axios.get(authUrl, { params: {login, password} })
-                .then(response => dispatch(updateAuthParam({ ...response.data })))
-                .catch(err => console.log(err))
-    }, [dispatch, login, password])
+    const { isAuth } = useSelector(authFormSelector)
 
     return (
         <Wrap bgimg={bgimg}>
@@ -52,41 +31,7 @@ const Section = ({
                     <p>{subTitle}</p>
                 </ItemText>
             </Fade>
-            {regButtons && 
-                <>
-                    <Fade clear>
-                        <AuthForm>
-                            <Fade cascade top>
-                                <TextField 
-                                    name='login'
-                                    value={login}
-                                    onChange={handleChange}
-                                    style={{ margin: '20px 20px 0px 20px' }} 
-                                    label='Логин' 
-                                />
-                                <TextField
-                                    name='password'
-                                    value={password}
-                                    onChange={handleChange}
-                                    style={{ margin: '10px 20px 20px 20px' }}
-                                    label='Пароль'
-                                    type='password'
-                                />
-                            </Fade>
-                        </AuthForm>
-                    </Fade>
-                    <Fade bottom>
-                        <ButtonGroup>
-                            <AuthButton onClick={handleClick}>
-                                <p style={{ opacity: 1, color: '#fff' }}>Войти</p>
-                            </AuthButton>
-                            <RegButton>
-                                <p>Зарегистрироваться</p>
-                            </RegButton>
-                        </ButtonGroup>
-                    </Fade>
-                </>
-            }
+            {regButtons && !isAuth && <AuthForm />}
         </Wrap>
     )
 }
