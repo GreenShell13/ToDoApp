85dfd64d12402022c00f286f3270a3fd957eb30d
GreenShell13 <vlad_samokatov@mail.ru>
2023-08-20 18:20:38 +0300

    Menu added in draft version

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -1,12 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/home/Home';
-import Materials from './pages/materials/Materials';
-import Monitor from './pages/Monitor/Monitor';
+import { Route, Routes } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import './App.css'
+
+import Home from './pages/home/Home'
+import Materials from './pages/materials/Materials'
+import Monitor from './pages/Monitor/Monitor'
+import { Menu } from './components/common/menu/index'
+
 
 function App() {
+  const { isAuth } = useSelector(state => state.auth)
+  
   return (
     <>
+      { isAuth === true && <Menu /> }
       <div className="body">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -15,7 +23,7 @@ function App() {
         </Routes>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
