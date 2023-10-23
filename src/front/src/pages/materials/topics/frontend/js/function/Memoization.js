77391307d22f7a01d263bb44370ad5412ac50422
GreenShell13 <vlad_samokatov@mail.ru>
2023-10-23 23:54:54 +0300

    Memoized sample

--- src/front/src/pages/materials/topics/frontend/js/function/Memoization.js
+++ src/front/src/pages/materials/topics/frontend/js/function/Memoization.js
@@ -1,7 +1,24 @@
+import { memoized } from "../../../../../../helpers/memoization";
+import { timed } from "../../../../../../helpers/timers";
 
+function largeCalculations (number) {
+    let result = number;
+    for (
+        let i = 1;
+        i < number;
+        result += i++
+    );
+    return result;
+}
 
 const Memoization = () => {
-
+    const bigNumber = 1000000000;
+    console.log(timed(largeCalculations)(bigNumber));
+    const memoizedLargeCalculations = memoized(largeCalculations);
+    console.log(timed(memoizedLargeCalculations)(bigNumber));
+    console.log(timed(memoizedLargeCalculations)(bigNumber));
+    console.log(timed(memoizedLargeCalculations)(bigNumber * 10));
+    console.log(timed(memoizedLargeCalculations)(bigNumber * 10));
 };
 
 export default Memoization;
